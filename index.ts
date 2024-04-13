4efbc980a00c246e2523ffa1cd43abe5a22f5134
Saeed Ahmed <165307721+sdad27@users.noreply.github.com>
2024-04-13 21:57:44 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let randomNumber = Math.floor(Math.random() * 10 + 1);
+
+let answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "guessedNumber",
+        message: "guess the number between 1-10",
+    }
+]);
+
+if (answer.guessedNumber === randomNumber) {
+    console.log("you guess the right number");
+    
+} else {
+    console.log("you guess the wrong number");
+}
